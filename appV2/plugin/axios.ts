8211303b7d18b7e4e6614763f169f7c0759f0001
Eugénio Cachiombo <120019370+eugeniocachiombo@users.noreply.github.com>
2026-03-15 19:04:15 +0100

    Simplify axios instance configuration
    Removed unnecessary configuration options from axios instance.

--- appV2/plugin/axios.ts
+++ appV2/plugin/axios.ts
@@ -2,12 +2,7 @@ import axiosInstace from 'axios';
 
 const axios:any = axiosInstace.create({
     baseURL: 'https://genioproapi.infinityfreeapp.com',
-    withCredentials: true,
-    withXSRFToken: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+   
 });
 
 export default axios;
