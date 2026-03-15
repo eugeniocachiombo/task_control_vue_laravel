fe28550e773effae4758948f379790b71faba746
Eugénio Cachiombo <120019370+eugeniocachiombo@users.noreply.github.com>
2026-03-15 14:04:53 +0100

    Change axios baseURL to production API endpoint

--- appV2/plugin/axios.ts
+++ appV2/plugin/axios.ts
@@ -1,7 +1,7 @@
 import axiosInstace from 'axios';
 
 const axios:any = axiosInstace.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://genioproapi.infinityfreeapp.com/',
     withCredentials: true,
     withXSRFToken: true,
     headers: {
