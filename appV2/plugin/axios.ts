096967028f5456c231e1f146c00c252c3232c709
Eugénio Cachiombo <120019370+eugeniocachiombo@users.noreply.github.com>
2026-03-15 19:05:59 +0100

    Configure axios instance with credentials and headers

--- appV2/plugin/axios.ts
+++ appV2/plugin/axios.ts
@@ -2,7 +2,12 @@ import axiosInstace from 'axios';
 
 const axios:any = axiosInstace.create({
     baseURL: 'https://genioproapi.infinityfreeapp.com',
-   
+    withCredentials: true,
+    withXSRFToken: true,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
 });
 
 export default axios;
