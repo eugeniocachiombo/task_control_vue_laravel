92d897bd83aa15675c2bf78b920d220cb964f179
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-04-02 21:14:08 +0100

    fix: correção dos imports javascript

--- appV2/routes/route.ts
+++ appV2/routes/route.ts
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  { path: '/:pathMatch(.*)*', component: import('@/error/Index.vue') },
-  { path: "/", component: import('@/login/Index.vue') },
-  { path: "/login", name: 'login', component: import('@/login/Index.vue') },
-  { path: "/criar-conta", name: 'signup', component: import('@/signup/Index.vue') },
+  { path: '/:pathMatch(.*)*', component: () => import('@/error/Index.vue') },
+  { path: "/", component: () => import('@/login/Index.vue') },
+  { path: "/login", name: 'login', component: () => import('@/login/Index.vue') },
+  { path: "/criar-conta", name: 'signup', component: () => import('@/signup/Index.vue') },
   {
     path: "/main",
-    component: () =>import('@/main/Index.vue'),
+    component: () => import('@/main/Index.vue'),
     children: [
-      { path: "/painel", name: 'dashboard', component: import('@/dashboard/Index.vue') },
+      { path: "/painel", name: 'dashboard', component: () => import('@/dashboard/Index.vue') },
     ]
   }
 ];
