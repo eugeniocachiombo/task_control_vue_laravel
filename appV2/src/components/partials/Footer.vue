76ae1780af36666216bfb5ca5af37efcb26ffa0e
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-04-03 16:51:02 +0100

    perf: powered by

--- appV2/src/components/partials/Footer.vue
+++ appV2/src/components/partials/Footer.vue
@@ -3,11 +3,9 @@
 <template>
     <footer class="footer" style="background-color: #222; color: white">
         <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a
-                    href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from
-                BootstrapDash.</span>
-            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All
-                rights reserved.</span>
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Directed by <a
+                    href="#" class="text-warning" target="_blank">Eugénio Cachiombo</a> </span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2026. Todos direitos reservados.</span>
         </div>
     </footer>
 </template>
