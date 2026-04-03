8d5fab87fc480fe91505d29b8a2d28a831e325b9
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-04-03 19:50:54 +0100

    perf: tema adicionado

--- appV2/src/main.ts
+++ appV2/src/main.ts
@@ -2,10 +2,18 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/routes/route';
 import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import ToastService from 'primevue/toastservice';
 
 createApp(App)
-.use(PrimeVue)
+.use(PrimeVue, {
+    theme: {
+      preset: Aura,
+      options: {
+        darkModeSelector: false
+      }
+    }
+  })
 .use(ToastService)
 .use(router)
 .mount('#app')
