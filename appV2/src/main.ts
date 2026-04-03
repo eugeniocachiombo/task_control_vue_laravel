21453f148cb943fd5e132d08bed15f52e8d87d5b
eugeniocachiombo <eugeniocachiombo@gmail.com>
2026-04-03 19:11:05 +0100

    feat: inserção primevue

--- appV2/src/main.ts
+++ appV2/src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/routes/route';
+import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(PrimeVue);
+.use(ToastService);
+.use(router)
+.mount('#app')
